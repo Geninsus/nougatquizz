feab2390c7ab2be61750af42df137a1a2e5ffc7e
Hugo MROCZKOWSKI <9fgj0c4c@randomail.net>
2017-11-29 17:32:12 +0100

    Add zero arg on random questions

--- projet-server/database.js
+++ projet-server/database.js
@@ -6,6 +6,17 @@ function compareNumbers (a, b) {
   return Number(a) - Number(b)
 }
 
+function shuffleArray (array) {
+  let myArray = array
+  for (var i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+    let temp = myArray[i]
+    myArray[i] = myArray[j]
+    myArray[j] = temp
+  }
+  return myArray
+}
+
 exports.init = () => {
   jsonfile.readFile('./questions.json', (err, data) => {
     if (!err) {
@@ -45,15 +56,18 @@ exports.getRandomQuestions = async (categorie, count) => {
   let tmp = []
   let i = 0
 
-  while (i < count) {
-    let newElt = avaliable[Math.floor(Math.random() * avaliable.length)]
-    if (tmp.indexOf(newElt) === -1) {
-      tmp.push(newElt)
-      i++
+  if (count > 0) {
+    while (i < count) {
+      let newElt = avaliable[Math.floor(Math.random() * avaliable.length)]
+      if (tmp.indexOf(newElt) === -1) {
+        tmp.push(newElt)
+        i++
+      }
     }
+    return tmp
+  } else {
+    return shuffleArray(avaliable)
   }
-
-  return tmp
 }
 
 exports.getBestScores = async (count) => {
